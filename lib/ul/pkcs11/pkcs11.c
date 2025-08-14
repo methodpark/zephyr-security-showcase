ba5fad85cc067366c41a281229f95ae2cc2b7680
Stefan Kraus <stefan.kraus@ul.com>
2025-08-14 20:57:04 +0200

    feat: implement subset of functions
    This change adds our implementations of the chosen subset of PKCS11.
    
    Right now, unit tests are missing, but will be added next.
    
    Signed-off-by: Stefan Kraus <stefan.kraus@ul.com>

--- lib/ul/pkcs11/pkcs11.c
+++ lib/ul/pkcs11/pkcs11.c
@@ -1,16 +1,47 @@
+#include <stdio.h>
+#include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+
+#include <psa/crypto.h>
+#include <psa/crypto_extra.h>
 
 #include <external/pkcs11/pkcs11.h>
 
 LOG_MODULE_REGISTER(ul_pkcs11, LOG_LEVEL_INF);
 
+typedef struct
+{
+    psa_key_id_t key_id;
+    psa_algorithm_t alg;
+    psa_key_type_t type;
+} pkcs11_crypto_context_t;
+
+static pkcs11_crypto_context_t g_ctx;
+
+static psa_cipher_operation_t g_encryptOp = PSA_CIPHER_OPERATION_INIT;
+static psa_cipher_operation_t g_decryptOp = PSA_CIPHER_OPERATION_INIT;
+
 CK_RV C_Initialize(CK_VOID_PTR pInitArgs)
 {
+    (void)pInitArgs;
+    psa_status_t status;
+
+    /* Initialize PSA Crypto */
+    status = psa_crypto_init();
+    if (status != PSA_SUCCESS)
+    {
+        LOG_ERR("psa_crypto_init failed! (Error: %d)", status);
+        return CKR_GENERAL_ERROR;
+    }
+
     return CKR_OK;
 }
 
 CK_RV C_Finalize(CK_VOID_PTR pInitArgs)
 {
+    (void)pInitArgs;
+    memset(&g_ctx, 0, sizeof(g_ctx));
+    LOG_INF("successfully finalized");
     return CKR_OK;
 }
 
@@ -20,6 +51,69 @@ CK_RV C_GenerateKey(CK_SESSION_HANDLE hSession,
                     CK_ULONG ulCount,
                     CK_OBJECT_HANDLE_PTR pObjHdl)
 {
+    (void)hSession;
+
+    // Only AES key generation supported in this mock
+    if (!pMechanism || pMechanism->mechanism != CKM_AES_KEY_GEN)
+    {
+        LOG_ERR("Unsupported mechanism: %lu", pMechanism->mechanism);
+        return CKR_MECHANISM_INVALID;
+    }
+
+    g_ctx = (pkcs11_crypto_context_t){
+        .key_id = PSA_KEY_ID_USER_MIN,
+        .alg = PSA_ALG_CTR,
+        .type = PSA_KEY_TYPE_AES
+    };
+
+    psa_key_handle_t handle;
+    psa_status_t status = psa_open_key(g_ctx.key_id, &handle);
+
+    if (status == PSA_SUCCESS)
+    {
+        psa_close_key(handle);
+        LOG_INF("Key already exists, stored in session context: %lu", (unsigned long)g_ctx.key_id);
+        return CKR_OK;
+    }
+    else if (status != PSA_ERROR_DOES_NOT_EXIST)
+    {
+        LOG_ERR("psa_open_key failed! (Error: %d)", status);
+        return CKR_FUNCTION_FAILED;
+    }
+
+    // Configure key attributes
+    psa_key_attributes_t key_attributes = PSA_KEY_ATTRIBUTES_INIT;
+    psa_set_key_usage_flags(&key_attributes, PSA_KEY_USAGE_ENCRYPT | PSA_KEY_USAGE_DECRYPT);
+    psa_set_key_algorithm(&key_attributes, g_ctx.alg);
+    psa_set_key_type(&key_attributes, g_ctx.type);
+    psa_set_key_bits(&key_attributes, 128);
+
+    // Persistent key specific settings
+    psa_set_key_lifetime(&key_attributes, PSA_KEY_LIFETIME_PERSISTENT);
+    psa_set_key_id(&key_attributes, PSA_KEY_ID_USER_MIN);
+
+    // Generate key
+    status = psa_generate_key(&key_attributes, &g_ctx.key_id);
+    if (status != PSA_SUCCESS)
+    {
+        LOG_INF("psa_generate_key failed! (Error: %d)", status);
+        psa_reset_key_attributes(&key_attributes);
+        return CKR_FUNCTION_FAILED;
+    }
+
+    // Purge from volatile memory
+    status = psa_purge_key(g_ctx.key_id);
+    if (status != PSA_SUCCESS)
+    {
+        LOG_INF("psa_purge_key failed! (Error: %d)", status);
+        psa_reset_key_attributes(&key_attributes);
+        return CKR_FUNCTION_FAILED;
+    }
+
+    psa_reset_key_attributes(&key_attributes);
+
+    LOG_INF("Persistent key generated successfully! ID: %lu", (unsigned long)g_ctx.key_id);
+
     return CKR_OK;
 }
 
@@ -28,6 +122,23 @@ CK_RV C_EncryptInit(CK_SESSION_HANDLE hSession,
                     CK_MECHANISM_PTR pMechanism,
                     CK_OBJECT_HANDLE hKey)
 {
+    (void)hSession;
+    psa_key_handle_t handle;
+    psa_status_t status = psa_open_key(g_ctx.key_id, &handle);
+
+    if (status != PSA_SUCCESS)
+    {
+        LOG_ERR("psa_open_key failed! (Error: %d)", status);
+        return CKR_FUNCTION_FAILED;
+    }
+
+    status = psa_cipher_encrypt_setup(&g_encryptOp, g_ctx.key_id, g_ctx.alg);
+    if (status != PSA_SUCCESS)
+    {
+        LOG_ERR("psa_cipher_encrypt_setup failed! (Error: %d)", status);
+        return CKR_FUNCTION_FAILED;
+    }
+
     return CKR_OK;
 }
 
@@ -37,7 +148,26 @@ CK_RV C_Encrypt(CK_SESSION_HANDLE hSession,
                 CK_BYTE_PTR pEncryptedData,
                 CK_ULONG_PTR pulEncryptedDataLen)
 {
-    return CKR_OK;
+    (void)hSession;
+    psa_key_handle_t handle;
+    psa_status_t status = psa_open_key(g_ctx.key_id, &handle);
+
+    if (status != PSA_SUCCESS)
+    {
+        LOG_ERR("psa_open_key failed! (Error: %d)", status);
+        return CKR_FUNCTION_FAILED;
+    }
+    status = psa_cipher_encrypt(g_ctx.key_id, g_ctx.alg,
+                                pData, ulDataLen,
+                                pEncryptedData, *pulEncryptedDataLen,
+                                (size_t *)pulEncryptedDataLen);
+    if (status != PSA_SUCCESS)
+    {
+        LOG_ERR("psa_cipher_encrypt failed! (Error: %d)", status);
+        return CKR_FUNCTION_FAILED;
+    }
+
+    return PSA_SUCCESS;
 }
 
 CK_RV C_EncryptUpdate(CK_SESSION_HANDLE hSession,
@@ -46,20 +176,61 @@ CK_RV C_EncryptUpdate(CK_SESSION_HANDLE hSession,
                       CK_BYTE_PTR pEncryptedPart,
                       CK_ULONG_PTR pulEncryptedPartLen)
 {
-    return CKR_OK;
+    (void)hSession;
+    psa_status_t status = psa_cipher_update(&g_encryptOp,
+                                            pPart, ulPartLen,
+                                            pEncryptedPart, *pEncryptedPart,
+                                            (size_t *)pulEncryptedPartLen);
+    if (status != PSA_SUCCESS)
+    {
+        LOG_ERR("psa_cipher_update failed! (Error: %d)", status);
+        return CKR_FUNCTION_FAILED;
+    }
+
+    return PSA_SUCCESS;
 }
 
 CK_RV C_EncryptFinal(CK_SESSION_HANDLE hSession,
                      CK_BYTE_PTR pLastEncryptedPart,
                      CK_ULONG_PTR pulLastEncryptedPartLen)
 {
-    return CKR_OK;
+    (void)hSession;
+    psa_status_t status = psa_cipher_finish(&g_encryptOp,
+                                            pLastEncryptedPart,
+                                            *pulLastEncryptedPartLen,
+                                            (size_t *)pulLastEncryptedPartLen);
+    psa_cipher_abort(&g_encryptOp);
+    if (status != PSA_SUCCESS)
+    {
+        LOG_ERR("psa_cipher_finish failed! (Error: %d)", status);
+        return CKR_FUNCTION_FAILED;
+    }
+
+    return PSA_SUCCESS;
 }
 
+// ---------- Decrypt ----------
 CK_RV C_DecryptInit(CK_SESSION_HANDLE hSession,
                     CK_MECHANISM_PTR pMechanism,
                     CK_OBJECT_HANDLE hKey)
 {
+    (void)hSession;
+    psa_key_handle_t handle;
+    psa_status_t status = psa_open_key(g_ctx.key_id, &handle);
+
+    if (status != PSA_SUCCESS)
+    {
+        LOG_ERR("psa_open_key failed! (Error: %d)", status);
+        return CKR_FUNCTION_FAILED;
+    }
+
+    status = psa_cipher_decrypt_setup(&g_decryptOp, g_ctx.key_id, g_ctx.alg);
+    if (status != PSA_SUCCESS)
+    {
+        LOG_ERR("psa_cipher_decrypt_setup failed! (Error: %d)", status);
+        return CKR_FUNCTION_FAILED;
+    }
+
     return CKR_OK;
 }
 
@@ -69,6 +240,24 @@ CK_RV C_Decrypt(CK_SESSION_HANDLE hSession,
                 CK_BYTE_PTR pData,
                 CK_ULONG_PTR pulDataLen)
 {
+    (void)hSession;
+    psa_key_handle_t handle;
+    psa_status_t status = psa_open_key(g_ctx.key_id, &handle);
+
+    if (status != PSA_SUCCESS)
+    {
+        LOG_ERR("psa_open_key failed! (Error: %d)", status);
+        return CKR_FUNCTION_FAILED;
+    }
+    status = psa_cipher_decrypt(g_ctx.key_id, g_ctx.alg, pEncryptedData,
+                                ulEncryptedDataLen, pData,
+                                *pData, (size_t *)pulDataLen);
+    if (status != PSA_SUCCESS)
+    {
+        LOG_ERR("psa_cipher_decrypt failed! (Error: %d)", status);
+        return CKR_FUNCTION_FAILED;
+    }
+
     return CKR_OK;
 }
 
@@ -78,6 +267,16 @@ CK_RV C_DecryptUpdate(CK_SESSION_HANDLE hSession,
                       CK_BYTE_PTR pPart,
                       CK_ULONG_PTR pulPartLen)
 {
+    (void)hSession;
+    psa_status_t status = psa_cipher_update(&g_decryptOp,
+                                            pEncryptedPart, ulEncryptedPartLen,
+                                            pPart, *pPart,
+                                            (size_t *)pulPartLen);
+    if (status != PSA_SUCCESS)
+    {
+        LOG_ERR("psa_cipher_update failed! (Error: %d)", status);
+        return CKR_FUNCTION_FAILED;
+    }
     return CKR_OK;
 }
 
@@ -85,5 +284,16 @@ CK_RV C_DecryptFinal(CK_SESSION_HANDLE hSession,
                      CK_BYTE_PTR pLastPart,
                      CK_ULONG_PTR pulLastPartLen)
 {
+    (void)hSession;
+    psa_status_t status = psa_cipher_finish(&g_decryptOp,
+                                            pLastPart, *pulLastPartLen,
+                                            (size_t *)pulLastPartLen);
+    psa_cipher_abort(&g_decryptOp);
+    if (status != PSA_SUCCESS)
+    {
+        LOG_ERR("psa_cipher_finish failed! (Error: %d)", status);
+        return CKR_FUNCTION_FAILED;
+    }
+
     return CKR_OK;
 }
--- tests/lib/ul/pksc11/unit/src/main.c
+++ tests/lib/ul/pksc11/unit/src/main.c
@@ -1,12 +1,67 @@
+#include <stdarg.h>
 #include <zephyr/ztest.h>
+#include <zephyr/fff.h>
+
+#include <psa/crypto.h>
+#include <psa/crypto_extra.h>
 
 #include <external/pkcs11/pkcs11.h>
 
-ZTEST(ul_pkcs11_unit_testsuite, test__initialize__dummy_call)
+DEFINE_FFF_GLOBALS
+
+FAKE_VALUE_FUNC(psa_status_t, psa_crypto_init)
+FAKE_VALUE_FUNC(psa_status_t, psa_close_key, psa_key_handle_t)
+FAKE_VALUE_FUNC(psa_status_t, psa_open_key, mbedtls_svc_key_id_t, psa_key_handle_t*)
+FAKE_VALUE_FUNC(psa_status_t, psa_generate_key, const psa_key_attributes_t*, psa_key_id_t*)
+FAKE_VOID_FUNC(psa_reset_key_attributes, psa_key_attributes_t*)
+FAKE_VALUE_FUNC(psa_status_t, psa_purge_key, psa_key_id_t)
+FAKE_VALUE_FUNC(psa_status_t, psa_cipher_encrypt_setup, psa_cipher_operation_t*, psa_key_id_t, psa_algorithm_t)
+FAKE_VALUE_FUNC(psa_status_t, psa_cipher_encrypt, psa_key_id_t, psa_algorithm_t, const uint8_t*, size_t, uint8_t*, size_t, size_t*)
+FAKE_VALUE_FUNC(psa_status_t, psa_cipher_update, psa_cipher_operation_t*, const uint8_t*, size_t, uint8_t*, size_t, size_t*)
+FAKE_VALUE_FUNC(psa_status_t, psa_cipher_finish, psa_cipher_operation_t*, uint8_t*, size_t, size_t*)
+FAKE_VALUE_FUNC(psa_status_t, psa_cipher_abort, psa_cipher_operation_t*)
+FAKE_VALUE_FUNC(psa_status_t, psa_cipher_decrypt_setup, psa_cipher_operation_t*, psa_key_id_t, psa_algorithm_t)
+FAKE_VALUE_FUNC(psa_status_t, psa_cipher_decrypt, psa_key_id_t, psa_algorithm_t, const uint8_t*, size_t, uint8_t*, size_t, size_t*)
+
+void z_log_minimal_printk(const char *fmt, ...)
 {
+    // Do nothing with it for now
+    (void)fmt;
+}
+
+static void reset_fakes_fixture(void *f){
+    RESET_FAKE(psa_crypto_init)
+    RESET_FAKE(psa_close_key)
+    RESET_FAKE(psa_open_key)
+    RESET_FAKE(psa_generate_key)
+    RESET_FAKE(psa_reset_key_attributes)
+    RESET_FAKE(psa_purge_key)
+    RESET_FAKE(psa_cipher_encrypt_setup)
+    RESET_FAKE(psa_cipher_encrypt)
+    RESET_FAKE(psa_cipher_update)
+    RESET_FAKE(psa_cipher_finish)
+    RESET_FAKE(psa_cipher_abort)
+    RESET_FAKE(psa_cipher_decrypt_setup)
+    RESET_FAKE(psa_cipher_decrypt)
+}
+
+
+ZTEST(ul_pkcs11_unit_testsuite, test__initialize__psa_init_works__returns_success)
+{
+    psa_crypto_init_fake.return_val = PSA_SUCCESS;
+
     CK_RV ret = C_Initialize(NULL);
 
     zassert_equal(ret, CKR_OK, "C_Initialize failed");
 }
 
-ZTEST_SUITE(ul_pkcs11_unit_testsuite, NULL, NULL, NULL, NULL, NULL);
+ZTEST(ul_pkcs11_unit_testsuite, test__initialize__psa_init_fails___returns_error)
+{
+    psa_crypto_init_fake.return_val = PSA_ERROR_COMMUNICATION_FAILURE;
+
+    CK_RV ret = C_Initialize(NULL);
+
+    zassert_equal(ret, CKR_GENERAL_ERROR, "C_Initialize failed");
+}
+
+ZTEST_SUITE(ul_pkcs11_unit_testsuite, NULL, NULL, reset_fakes_fixture, NULL, NULL);
