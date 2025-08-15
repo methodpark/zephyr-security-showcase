510c7cc693666fcc0ee72ac0b0dcaa0741fc7af1
Stefan Kraus <stefan.kraus@ul.com>
2025-08-15 18:00:24 +0200

    feat: implement usage sample for PKCS11
    We add a small usage sample already how to use our library.
    
    Signed-off-by: Stefan Kraus <stefan.kraus@ul.com>

--- samples/ul/pkcs11/src/main.c
+++ samples/ul/pkcs11/src/main.c
@@ -1,3 +1,74 @@
-int main() {
-    return 0;
+#include <stddef.h>
+
+#include <zephyr/logging/log.h>
+
+#include <external/pkcs11/pkcs11.h>
+
+const int APP_SUCCESS = 0;
+const int APP_ERROR = -1;
+
+LOG_MODULE_REGISTER(persistent_key_usage, LOG_LEVEL_INF);
+
+int main(void)
+{
+    CK_RV rv;
+    CK_SESSION_HANDLE session = 0;
+    CK_MECHANISM mechanism = {CKM_AES_KEY_GEN, NULL, 0};
+    CK_OBJECT_HANDLE key_handle = 0;
+
+    // Init
+    rv = C_Initialize(NULL);
+    if (rv != CKR_OK)
+    {
+        LOG_ERR("C_Initialize failed: 0x%lX", rv);
+        return APP_ERROR;
+    }
+    LOG_INF("C_Initialize succeeded");
+
+    // Generate/reuse key
+    rv = C_GenerateKey(session, &mechanism, NULL, 0, &key_handle);
+    if (rv != CKR_OK)
+    {
+        LOG_ERR("C_GenerateKey failed: 0x%lX", rv);
+        return APP_ERROR;
+    }
+    LOG_INF("C_GenerateKey succeeded");
+
+    // Encryption test
+    CK_BYTE plaintext[] = "Sample text to be encrypted!";
+    LOG_INF("Plaintext: %s", plaintext);
+    CK_BYTE ciphertext[64];
+    CK_ULONG ciphertext_len = sizeof(ciphertext);
+
+    rv = C_Encrypt(session, plaintext, sizeof(plaintext), ciphertext, &ciphertext_len);
+    if (rv != CKR_OK)
+    {
+        LOG_ERR("C_Encrypt failed: 0x%lX", rv);
+        return APP_ERROR;
+    }
+    LOG_INF("Ciphertext length: %lu", ciphertext_len);
+
+    // Decryption test
+    CK_BYTE decrypted[64];
+    CK_ULONG decrypted_len = sizeof(decrypted);
+
+    rv = C_Decrypt(session, ciphertext, ciphertext_len, decrypted, &decrypted_len);
+    if (rv != CKR_OK)
+    {
+        LOG_ERR("C_Dencrypt failed: 0x%lX", rv);
+        return APP_ERROR;
+    }
+
+
+    LOG_INF("Decrypted: %s", decrypted);
+
+    // Finalize
+    rv = C_Finalize(NULL);
+    if (rv != CKR_OK)
+    {
+        LOG_ERR("C_Finalize failed: 0x%lX", rv);
+        return APP_ERROR;
+    }
+
+    return APP_SUCCESS;
 }
