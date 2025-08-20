1eee71ebc5df3f4e16f480cbf8ed0f3349ef4ecc
Stefan Kraus <stefan.kraus@ul.com>
2025-08-20 09:38:36 +0200

    feat: add unit test for pkcs11 finalize method
    Signed-off-by: Stefan Kraus <stefan.kraus@ul.com>

--- tests/lib/ul/pksc11/unit/src/main.c
+++ tests/lib/ul/pksc11/unit/src/main.c
@@ -53,7 +53,7 @@ static void reset_sut(){
 
 static void setup_before_test_fixture(void *f){
     reset_fakes();
-    //reset_sut();
+    reset_sut();
 }
 
 ZTEST(ul_pkcs11_unit_testsuite, test__initialize__psa_init_works__returns_success)
@@ -72,6 +72,43 @@ ZTEST(ul_pkcs11_unit_testsuite, test__initialize__psa_init_fails___returns_error
     CK_RV ret = C_Initialize(NULL);
 
     zassert_equal(ret, CKR_GENERAL_ERROR, "C_Initialize failed");
+}
+
+ZTEST(ul_pkcs11_unit_testsuite, test__finalize__works_and_zero){
+    pkcs11_crypto_context_t *ctx = get_crypto_ctx_for_tests();
+
+    ctx->key_id = 1;
+    ctx->alg = 2;
+    ctx->type = 3;
+
+    CK_RV ret = C_Finalize(NULL);
+
+    zassert_equal(ret, CKR_OK, "C_Finalize failed");
+    zassert_equal(ctx->key_id, 0, "Key ID not reset");
+    zassert_equal(ctx->alg, 0, "Alg not reset");
+    zassert_equal(ctx->type, 0, "Type not reset");
+}
+
+ZTEST(ul_pkcs11_unit_testsuite, test__generate_key__pMechanism_nullptr__returns_arguments_bad)
+{
+    CK_RV ret = C_GenerateKey(0, NULL, NULL, 0, NULL);
+
+    zassert_equal(ret, CKR_ARGUMENTS_BAD, "C_GenerateKey did not respond CKR_ARGUMENTS_BAD on NULL mechanism");
+}
+
+ZTEST(ul_pkcs11_unit_testsuite, test__generate_key__invalid_mechanism__returns_mechanism_invalid)
+{
+    CK_MECHANISM mechanism = {
+        .mechanism = CKM_AES_XTS_KEY_GEN,
+    };
+
+    CK_RV ret = C_GenerateKey(0, &mechanism, NULL, 0, NULL);
+
+    zassert_equal(ret, CKR_MECHANISM_INVALID, "C_GenerateKey did not reject non-AES keygen");
+}
+
+ZTEST(ul_pkcs11_unit_testsuite, test__generate_key__open_key_fails__returns_all_functions_works__returns_success){
+
 }
 
 ZTEST_SUITE(ul_pkcs11_unit_testsuite, NULL, NULL, setup_before_test_fixture, NULL, NULL);
