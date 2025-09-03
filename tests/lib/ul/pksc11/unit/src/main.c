09ad94fb15b328d6baffc28ff6da22a7d71088d3
Stefan Kraus <stefan.kraus@ul.com>
2025-09-03 11:32:10 +0200

    chore: add unit tests for C_GenerateKey
    Signed-off-by: Stefan Kraus <stefan.kraus@ul.com>

--- tests/lib/ul/pksc11/unit/src/main.c
+++ tests/lib/ul/pksc11/unit/src/main.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdarg.h>
 #include <zephyr/ztest.h>
 #include <zephyr/fff.h>
@@ -89,6 +90,47 @@ ZTEST(ul_pkcs11_unit_testsuite, test__finalize__works_and_zero){
     zassert_equal(ctx->type, 0, "Type not reset");
 }
 
+
+// To capture the attributes passed to psa_generate_key
+static psa_key_attributes_t copied_attrs;
+
+static psa_status_t psa_gen_key_capture_attrs(const psa_key_attributes_t* attributes, psa_key_id_t* key) {
+
+    assert(attributes != NULL);
+    assert(key != NULL);
+
+    copied_attrs = *attributes;
+    *key = 0x857883;
+
+    return PSA_SUCCESS;
+}
+
+
+ZTEST(ul_pkcs11_unit_testsuite, test__generate_key__new_key_psa_functions_succeed__returns_success){
+    psa_open_key_fake.return_val = PSA_ERROR_DOES_NOT_EXIST;
+    psa_generate_key_fake.custom_fake = psa_gen_key_capture_attrs;
+    psa_purge_key_fake.return_val = PSA_SUCCESS;
+
+    CK_MECHANISM mechanism = {
+        .mechanism = CKM_AES_KEY_GEN,
+    };
+
+    CK_RV ret = C_GenerateKey(0, &mechanism, NULL, 0, NULL);
+
+    zassert_equal(ret, CKR_OK, "C_GenerateKey failed");
+
+    zassert_equal(psa_open_key_fake.call_count, 1, "psa_open_key not called");
+    zassert_equal(psa_generate_key_fake.call_count, 1, "psa_generate_key not called");
+    zassert_equal(psa_purge_key_fake.call_count, 1, "psa_purge_key not called");
+
+    zassert_equal(psa_get_key_type(&copied_attrs), PSA_KEY_TYPE_AES, "Key type incorrect");
+    zassert_equal(psa_get_key_bits(&copied_attrs), 128, "Key bits incorrect");
+    zassert_equal(psa_get_key_usage_flags(&copied_attrs), PSA_KEY_USAGE_ENCRYPT | PSA_KEY_USAGE_DECRYPT, "Key usage incorrect");
+    zassert_equal(psa_get_key_algorithm(&copied_attrs), PSA_ALG_CTR, "Key algorithm incorrect");
+    zassert_equal(psa_get_key_lifetime(&copied_attrs), PSA_KEY_LIFETIME_PERSISTENT, "Key lifetime incorrect");
+    zassert_equal(psa_get_key_id(&copied_attrs), PSA_KEY_ID_USER_MIN, "Key ID incorrect");
+}
+
 ZTEST(ul_pkcs11_unit_testsuite, test__generate_key__pMechanism_nullptr__returns_arguments_bad)
 {
     CK_RV ret = C_GenerateKey(0, NULL, NULL, 0, NULL);
@@ -107,8 +149,70 @@ ZTEST(ul_pkcs11_unit_testsuite, test__generate_key__invalid_mechanism__returns_m
     zassert_equal(ret, CKR_MECHANISM_INVALID, "C_GenerateKey did not reject non-AES keygen");
 }
 
-ZTEST(ul_pkcs11_unit_testsuite, test__generate_key__open_key_fails__returns_all_functions_works__returns_success){
+ZTEST(ul_pkcs11_unit_testsuite, test__generate_key__key_already_exists__returns_success){
 
+    psa_open_key_fake.return_val = PSA_SUCCESS;
+    psa_close_key_fake.return_val = PSA_SUCCESS;
+
+    CK_MECHANISM mechanism = {
+        .mechanism = CKM_AES_KEY_GEN,
+    };
+
+    CK_RV ret = C_GenerateKey(0, &mechanism, NULL, 0, NULL);
+
+    zassert_equal(ret, CKR_OK, "C_GenerateKey failed");
+
+    zassert_equal(psa_open_key_fake.call_count, 1, "psa_open_key not called");
+    zassert_equal(psa_close_key_fake.call_count, 1, "psa_close_key not called");
+    zassert_equal(psa_generate_key_fake.call_count, 0, "psa_generate_key called unexpectedly");
+}
+
+ZTEST(ul_pkcs11_unit_testsuite, test__generate_key__psa_open_key_fails_unexpectedly__returns_function_failed){
+    psa_open_key_fake.return_val = PSA_ERROR_COMMUNICATION_FAILURE;
+
+    CK_MECHANISM mechanism = {
+        .mechanism = CKM_AES_KEY_GEN,
+    };
+
+    CK_RV ret = C_GenerateKey(0, &mechanism, NULL, 0, NULL);
+
+    zassert_equal(ret, CKR_FUNCTION_FAILED, "C_GenerateKey did not fail");
+
+    zassert_equal(psa_open_key_fake.call_count, 1, "psa_open_key not called");
+    zassert_equal(psa_generate_key_fake.call_count, 0, "psa_generate_key called unexpectedly");
+}
+
+ZTEST(ul_pkcs11_unit_testsuite, test__generate_key__psa_generate_key_fails__returns_function_failed){
+    psa_open_key_fake.return_val = PSA_ERROR_DOES_NOT_EXIST;
+    psa_generate_key_fake.return_val = PSA_ERROR_COMMUNICATION_FAILURE;
+
+    CK_MECHANISM mechanism = {
+        .mechanism = CKM_AES_KEY_GEN,
+    };
+
+    CK_RV ret = C_GenerateKey(0, &mechanism, NULL, 0, NULL);
+
+    zassert_equal(ret, CKR_FUNCTION_FAILED, "C_GenerateKey did not fail");
+
+    zassert_equal(psa_reset_key_attributes_fake.call_count, 1, "psa_reset_key_attributes not called");
+    zassert_equal(psa_purge_key_fake.call_count, 0, "psa_purge_key called unexpectedly");
+}
+
+ZTEST(ul_pkcs11_unit_testsuite, test__generate_key__psa_purge_key_fails__returns_function_failed){
+    psa_open_key_fake.return_val = PSA_ERROR_DOES_NOT_EXIST;
+    psa_generate_key_fake.return_val = PSA_SUCCESS;
+    psa_purge_key_fake.return_val = PSA_ERROR_COMMUNICATION_FAILURE;
+
+    CK_MECHANISM mechanism = {
+        .mechanism = CKM_AES_KEY_GEN,
+    };
+
+    CK_RV ret = C_GenerateKey(0, &mechanism, NULL, 0, NULL);
+
+    zassert_equal(ret, CKR_FUNCTION_FAILED, "C_GenerateKey did not fail");
+
+    zassert_equal(psa_reset_key_attributes_fake.call_count, 1, "psa_reset_key_attributes not called");
+    zassert_equal(psa_purge_key_fake.call_count, 1, "psa_purge_key not called");
 }
 
 ZTEST_SUITE(ul_pkcs11_unit_testsuite, NULL, NULL, setup_before_test_fixture, NULL, NULL);
