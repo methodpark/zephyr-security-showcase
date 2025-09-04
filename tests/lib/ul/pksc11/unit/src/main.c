d0d8b6639905d043e0b436c0ac9aa0d51a4a6b6f
Stefan Kraus <stefan.kraus@ul.com>
2025-09-04 15:35:24 +0200

    test: add some unit test for pkcs11 encrypt
    Signed-off-by: Stefan Kraus <stefan.kraus@ul.com>

--- tests/lib/ul/pksc11/unit/src/main.c
+++ tests/lib/ul/pksc11/unit/src/main.c
@@ -26,6 +26,9 @@ FAKE_VALUE_FUNC(psa_status_t, psa_cipher_abort, psa_cipher_operation_t*)
 FAKE_VALUE_FUNC(psa_status_t, psa_cipher_decrypt_setup, psa_cipher_operation_t*, psa_key_id_t, psa_algorithm_t)
 FAKE_VALUE_FUNC(psa_status_t, psa_cipher_decrypt, psa_key_id_t, psa_algorithm_t, const uint8_t*, size_t, uint8_t*, size_t, size_t*)
 
+#define DATA_LEN 16
+#define ENCRYPTED_BUFFER_LEN 32
+
 void z_log_minimal_printk(const char *fmt, ...)
 {
     // Do nothing with it for now
@@ -111,6 +114,28 @@ static psa_status_t psa_gen_key_capture_attrs(const psa_key_attributes_t* attrib
     copied_attrs = *attributes;
     *key = 0x857883;
 
+    return PSA_SUCCESS;
+}
+
+static psa_status_t psa_cipher_encrypt_fake_impl(mbedtls_svc_key_id_t key,
+                                                psa_algorithm_t alg,
+                                                const uint8_t *input,
+                                                size_t input_length,
+                                                uint8_t *output,
+                                                size_t output_size,
+                                                size_t *output_length)
+{
+
+    assert(input != NULL);
+    assert(output != NULL);
+    assert(output_length != NULL);
+
+    assert(output_size >= 3);
+
+    *output_length = 3;
+    output[0] = 0xAB;
+    output[1] = 0xCD;
+    output[2] = 0xEF;
     return PSA_SUCCESS;
 }
 
@@ -246,6 +271,63 @@ ZTEST(ul_pkcs11_unit_testsuite, test__encrypt_init__psa_functions_succeed__retur
 
     zassert_equal(psa_cipher_encrypt_setup_fake.arg1_val, GLOBAL_KEY_ID, "psa_cipher_encrypt_setup called with wrong key ID");
     zassert_equal(psa_cipher_encrypt_setup_fake.arg2_val, PSA_ALG_CTR, "psa_cipher_encrypt_setup called with wrong algorithm");
+}
+
+ZTEST(ul_pkcs11_unit_testsuite, test__encrypt__psa_cipher_encrypt_fails__returns_function_failed){
+    psa_cipher_encrypt_fake.return_val = PSA_ERROR_COMMUNICATION_FAILURE;
+
+    manually_initialize_global_crypto_context();
+
+    uint8_t data[DATA_LEN] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    uint8_t encrypted[ENCRYPTED_BUFFER_LEN] = {0};
+
+    long unsigned int encrypted_buffer_len = sizeof(encrypted);
+
+    uint8_t data_safe[DATA_LEN];
+    memcpy(data_safe, data, sizeof(data));
+
+    CK_RV ret = C_Encrypt(0, data, sizeof(data), encrypted, &encrypted_buffer_len);
+
+    zassert_equal(ret, CKR_FUNCTION_FAILED, "C_Encrypt did not fail");
+    zassert_equal(psa_cipher_encrypt_fake.call_count, 1, "psa_cipher_encrypt not called");
+
+    zassert_mem_equal(encrypted, (uint8_t[ENCRYPTED_BUFFER_LEN]){0}, ENCRYPTED_BUFFER_LEN, "Encrypted data unexpectedly written on fail");
+
+    zassert_mem_equal(data, data_safe, sizeof(data), "Input data was modified unexpectedly");
+    zassert_equal(encrypted_buffer_len, ENCRYPTED_BUFFER_LEN, "Encrypted buffer length was modified unexpectedly");
+}
+
+ZTEST(ul_pkcs11_unit_testsuite, test__encrypt__psa_cipher_encrypt_succeeds__returns_success){
+    psa_cipher_encrypt_fake.custom_fake = psa_cipher_encrypt_fake_impl;
+
+    manually_initialize_global_crypto_context();
+
+    uint8_t data[DATA_LEN] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    uint8_t encrypted[ENCRYPTED_BUFFER_LEN] = {0};
+    uint8_t expected_encrypted[ENCRYPTED_BUFFER_LEN] = {0xAB, 0xCD, 0xEF};
+
+    long unsigned int encrypted_buffer_len = sizeof(encrypted);
+
+    uint8_t data_safe[DATA_LEN];
+    memcpy(data_safe, data, sizeof(data));
+
+    CK_RV ret = C_Encrypt(0, data, sizeof(data), encrypted, &encrypted_buffer_len);
+
+    zassert_equal(ret, CKR_OK, "C_Encrypt did fail");
+
+    zassert_mem_equal(encrypted, expected_encrypted, ENCRYPTED_BUFFER_LEN, "Encrypted data was not correctly written");
+    zassert_equal(encrypted_buffer_len, 3, "Encrypted buffer length was not set correctly");
+
+    zassert_mem_equal(data, data_safe, sizeof(data), "Input data was modified unexpectedly");
+
+    zassert_equal(psa_cipher_encrypt_fake.call_count, 1, "psa_cipher_encrypt not called");
+    zassert_equal(psa_cipher_encrypt_fake.arg0_val, GLOBAL_KEY_ID, "psa_cipher_encrypt called with wrong key ID");
+    zassert_equal(psa_cipher_encrypt_fake.arg1_val, PSA_ALG_CTR, "psa_cipher_encrypt called with wrong algorithm");
+    zassert_equal_ptr(psa_cipher_encrypt_fake.arg2_val, (uintptr_t)data, "psa_cipher_encrypt called with wrong data pointer");
+    zassert_equal(psa_cipher_encrypt_fake.arg3_val, sizeof(data), "psa_cipher_encrypt called with wrong data length");
+    zassert_equal_ptr(psa_cipher_encrypt_fake.arg4_val, (uintptr_t)encrypted, "psa_cipher_encrypt called with wrong encrypted data pointer");
+    zassert_equal(psa_cipher_encrypt_fake.arg5_val, ENCRYPTED_BUFFER_LEN, "psa_cipher_encrypt called with wrong encrypted data buffer length");
+    zassert_equal_ptr(psa_cipher_encrypt_fake.arg6_val, (uintptr_t)&encrypted_buffer_len, "psa_cipher_encrypt called with wrong encrypted data length pointer");
 }
 
 ZTEST_SUITE(ul_pkcs11_unit_testsuite, NULL, NULL, setup_before_test_fixture, NULL, NULL);
